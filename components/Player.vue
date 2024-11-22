6eca39d688cc6ded7850e3100d2aaa9674e40895
Bertold Krausz <bertold.krausz@aldi-sued.com>
2024-11-22 16:46:30 +0100

    create player component

---
+++ components/Player.vue
@@ -0,0 +1,24 @@
+<template>
+   <div :class="[{ 'outline': player.card}]">
+        {{ player.name }}
+   </div>
+</template>
+
+<script>
+export default {
+    props: {
+        player: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+.outline {
+    border: 1px solid black;
+    padding: 10px;
+    margin: 10px;
+}
+</style>
