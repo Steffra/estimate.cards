3998615c5563d436e95851872308dbb71c241f02
Bertold Krausz <bertold.krausz@aldi-sued.com>
2024-11-22 16:30:27 +0100

    move websocket stuff out of the component

---
+++ composables/useWebsocket.ts
@@ -0,0 +1,33 @@
+export function useWebsocket() {
+    type Player = {
+        id: string,
+        name: string,
+        card: string | null,
+      }
+      
+      type Session = {
+        id: string,
+        cardsVisible: boolean,
+        players: Player[] 
+      }
+      const protocol= window.location.protocol === 'https:' ? 'wss' : 'ws'
+      const sessionid = window.location.pathname.split('/').pop()
+      const name = window.location.search.split('name=')[1] || window.sessionStorage.getItem('name')
+      if (!name) {
+        window.location.href = '/join'
+      }
+      const { status, data, send, open, close } = useWebSocket(`${protocol}://${location.host}/api/websocket?session=${sessionid}&name=${name}`)
+      const session = ref<Session>({ id: '', cardsVisible: false, players: [] })
+      
+      watch(data, (newValue) => {
+        session.value = JSON.parse(newValue)
+      
+        if (session.value.id) {
+          window.history.pushState({}, '', `/session/${session.value.id}`)
+        }
+      })
+
+    return {
+        session
+    };
+}
--- pages/[...session].vue
+++ pages/[...session].vue
@@ -1,42 +1,15 @@
 <script setup lang="ts">
-type Player = {
-  id: string,
-  name: string,
-  card: string | null,
-}
-
-type Session = {
-  id: string,
-  cardsVisible: boolean,
-  players: Player[] 
-}
-const protocol= window.location.protocol === 'https:' ? 'wss' : 'ws'
-const sessionid = window.location.pathname.split('/').pop()
-const name = window.location.search.split('name=')[1] || window.sessionStorage.getItem('name')
-if (!name) {
-  window.location.href = '/join'
-}
-const { status, data, send, open, close } = useWebSocket(`${protocol}://${location.host}/api/websocket?session=${sessionid}&name=${name}`)
-const history = ref<string[]>([])
-const session = ref<Session>({ id: '', cardsVisible: false, players: [] })
-
-watch(data, (newValue) => {
-  history.value.push(`${newValue}`)
-  session.value = JSON.parse(newValue)
-
-  if (session.value.id) {
-    window.history.pushState({}, '', `/session/${session.value.id}`)
-  }
-})
-
-
+import { useWebsocket } from '~/composables/useWebsocket'
+const socket = useWebsocket()
 </script>
 
 <template>
   <div>
-   <h1>Session: {{ session.id }}</h1>
+   <h1>Session: {{ socket.session.value.id }}</h1>
     <div>
-      <p v-for="player in session.players">{{ player.name }}</p>
+      <p v-for="player in socket.session.value.players" :key="player.id">
+        {{ player.name }}
+      </p>
     </div>
   </div>
 </template>
