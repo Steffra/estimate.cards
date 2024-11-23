2dd2a28604c243fdbcea96c8a29cfb4d50a3075b
Bertold Krausz <bertold.krausz@aldi-sued.com>
2024-11-23 15:14:15 +0100

    add a bit of css to player cards

--- components/Player.vue
+++ components/Player.vue
@@ -1,7 +1,12 @@
 <template>
-   <div :class="[{ 'outline': player.card}]">
-        {{ player.name }}
-   </div>
+    <div class="player">
+        <div class="player-card" :class="[{ 'voted': player.card}]">
+                
+        </div>
+        <div class="player-name">{{ player.name }}</div>
+
+    </div>
+    
 </template>
 
 <script setup lang="ts">
@@ -12,9 +17,23 @@ const { player } = defineProps<{
 </script>
 
 <style scoped>
-.outline {
+.player {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    min-width: 100px;
+}
+
+.player-card {
     border: 1px solid black;
-    padding: 10px;
-    margin: 10px;
+    border-radius: 5px;
+    min-width: 80px;
+    min-height: 120px;
+    background-color: wheat;
+}
+.voted {
+    background-color: lightgreen;    
+
 }
 </style>
--- pages/[...session].vue
+++ pages/[...session].vue
@@ -6,16 +6,25 @@ const socket = useWebsocket()
 <template>
   <div>
    <h1>Session: {{ socket.session.value.id }}</h1>
-    <div>
+    <div class="players">
       <p v-for="player in socket.session.value.players" :key="player.id">
         <Player :player="player" />
       </p>
-      <button @click="socket.vote('1')">1</button>
+      
+    </div>
+    <button @click="socket.vote('1')">1</button>
       <button @click="socket.vote('2')">2</button>
       <button @click="socket.vote('3')">3</button>
       <button @click="socket.vote('5')">5</button>
       <button @click="socket.vote('8')">8</button>
       <button @click="socket.vote('13')">13</button>
-    </div>
   </div>
 </template>
+
+<style>
+  .players {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+  }
+</style>
