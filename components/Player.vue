23cad60e1efa14ee4731234543d26c40a68578e7
Bertold Krausz <bertold.krausz@aldi-sued.com>
2024-11-24 20:31:38 +0100

    add animation when selectig a card

--- components/Player.vue
+++ components/Player.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="player">
-        <div class="player-card" :class="[{ 'voted': player.card}]">
-                
+    <div :id="player.id" class="player">
+        <div class="player-card" :class="[{ 'voted': player.card}]" ref="card">
+                 
         </div>
         <div class="player-name">{{ player.name }}</div>
 
@@ -11,6 +11,33 @@
 
 <script setup lang="ts">
 import { type Player } from '~/types/types';
+import { animate, spring } from "motion"
+
+const card = ref<HTMLElement | null>(null);
+
+watch(() => player.card, (newCard, oldCard) => {
+    if (player.card && !oldCard) {
+        animateCard();
+    }
+});
+
+const animateCard = () => {
+    const cardelement = document.getElementById(player.id);
+    animate(
+        cardelement!,
+        { transform: "translateY(-10px)" },
+        { duration: 0.2 }
+        
+    ).then(() => {
+        animate(
+            cardelement!,
+            { transform: "translateY(0px)" },
+            { type: spring, stiffness: 800 }
+        );
+    });
+
+};
+
 const { player } = defineProps<{
   player: Player
 }>()
@@ -23,17 +50,21 @@ const { player } = defineProps<{
     align-items: center;
     justify-content: center;
     min-width: 100px;
+    gap: 8px;
 }
 
 .player-card {
-    border: 1px solid black;
     border-radius: 5px;
+    box-shadow: 5px 5px 5px;
     min-width: 80px;
     min-height: 120px;
+
     background-color: wheat;
+    color: rgb(126, 115, 93);
 }
 .voted {
     background-color: lightgreen;    
+    color: rgb(51, 83, 51);
 
 }
 </style>
--- pages/[...session].vue
+++ pages/[...session].vue
@@ -6,19 +6,20 @@ const socket = useWebsocket()
 <template>
   <div>
    <h1>Session: {{ socket.session.value.id }}</h1>
-    <div class="players">
-      <p v-for="player in socket.session.value.players" :key="player.id">
-        <Player :player="player" />
-      </p>
-      
-    </div>
-    <button @click="socket.vote('1')">1</button>
+   <button @click="socket.vote('1')">1</button>
       <button @click="socket.vote('2')">2</button>
       <button @click="socket.vote('3')">3</button>
       <button @click="socket.vote('5')">5</button>
       <button @click="socket.vote('8')">8</button>
       <button @click="socket.vote('13')">13</button>
   </div>
+  <div class="players">
+      <p v-for="player in socket.session.value.players" :key="player.id">
+        <Player :player="player" />
+      </p>
+      
+    </div>
+    
 </template>
 
 <style>
