c79bde94055aabf0ea3ce29aafe13cbac6fae537
Bertold Krausz <bertold.krausz@aldi-sued.com>
2024-12-18 08:30:48 +0100

    add meta to prevent zooming on mobile when entering an input field

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,12 @@ export default defineNuxtConfig({
       websocket: true
     }
   },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+    }
+  },
   ssr: false,
 
   compatibilityDate: '2024-11-19'
