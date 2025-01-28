fad7ce85fe23024efce75858c4e20548de0e5719
Bertold Krausz <bertold.krausz@aldi-sued.com>
2025-01-28 22:58:32 +0100

    add animation when everyone voted

--- components/ToggleButton.vue
+++ components/ToggleButton.vue
@@ -1,14 +1,40 @@
 <template>
-    <BaseButton class="toggle-button" :text="cardsVisible ? 'NEW ROUND' : 'REVEAL CARDS'"
+    <BaseButton ref="toggleButton" class="toggle-button" :text="cardsVisible ? 'NEW ROUND' : 'REVEAL CARDS'"
         :secondary="cardsVisible || somePlayersVoted" :disabled="noPlayersVoted" />
 </template>
 
 <script setup lang="ts">
+import { animate, spring } from "motion"
 const { cardsVisible, somePlayersVoted, noPlayersVoted } = defineProps<{
     cardsVisible: boolean,
     somePlayersVoted: boolean
     noPlayersVoted: boolean
 }>()
+const buttonRef = useTemplateRef('toggleButton')
+
+watchEffect(() => {
+    if (!cardsVisible && !noPlayersVoted && !somePlayersVoted) {
+        animateToggleButton()
+    }
+})
+
+
+const animateToggleButton = () => {
+    const animationSequence = [
+        [
+            buttonRef.value?.$el,
+            { transform: "translateX(-25px) " },
+            { duration: 0.5 }
+        ],
+        [
+            buttonRef.value?.$el,
+            { transform: "translateX(0px)" },
+            { duration: 0.5, type: spring, stiffness: 600 }
+        ]
+    ]
+    // @ts-ignore something is wrong with the typing in Motion
+    animate(animationSequence)
+};
 </script>
 
 <style scoped>
