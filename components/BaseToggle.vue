29aab678750d2d27dc2ef44c1a1e91848ddedf40
Bertold Krausz <bertold.krausz@aldi-sued.com>
2025-02-02 17:37:05 +0100

    add toggle button component

---
+++ components/BaseToggle.vue
@@ -0,0 +1,102 @@
+<template>
+    <label for="toggleinput" class="wrapper">
+        <div class="text">
+            {{ text }}
+        </div>
+        <div class="toggle-container">
+            <div class="toggle" for="toggleinput">
+                <input id="toggleinput" class="toggle__input" type="checkbox" :checked="value"
+                    @change="$emit('toggle')" />
+                <div class=" toggle__slider">
+                </div>
+            </div>
+            <span class="label">{{ inputValue ? 'ON' : 'OFF' }}</span>
+        </div>
+    </label>
+</template>
+
+<script setup lang="ts">
+const inputValue = defineModel('inputValue')
+const { text, value } = defineProps<{
+    text: string
+    value: boolean
+}>()
+
+defineEmits<{
+    toggle: []
+}>()
+
+</script>
+
+<style scoped>
+.wrapper {
+    display: flex;
+    align-items: end;
+    flex-direction: column;
+    gap: 12px;
+    width: 200px;
+
+    .text {
+        font-size: 11px;
+        font-weight: bold;
+        color: darkgrey;
+        cursor: pointer;
+    }
+
+    .toggle-container {
+        display: flex;
+        align-items: center;
+        gap: 5px;
+        width: 100px;
+
+        .label {
+            font-size: 11px;
+            font-weight: bold;
+            color: darkgrey;
+        }
+
+        .toggle {
+            display: inline-block;
+            cursor: pointer;
+            height: 26px;
+            width: 50px;
+            padding-left: 10px;
+
+            .toggle__input {
+                display: none;
+
+                &:checked+.toggle__slider {
+                    background-color: #02b5db;
+
+                    &:before {
+                        left: 22px;
+                    }
+                }
+            }
+
+            .toggle__slider {
+                width: 40px;
+                height: 20px;
+                background: #ccc;
+                padding: 3px;
+                border-radius: 2em;
+                position: absolute;
+                transition: 300ms cubic-bezier(0.785, 0.135, 0.150, 0.860);
+
+                &:before {
+                    content: '';
+                    width: 20px;
+                    height: 20px;
+                    background: #fff;
+                    position: absolute;
+                    top: 50%;
+                    left: 5px;
+                    transform: translateY(-50%);
+                    border-radius: 50%;
+                    transition: 300ms cubic-bezier(0.785, 0.135, 0.150, 0.860);
+                }
+            }
+        }
+    }
+}
+</style>
